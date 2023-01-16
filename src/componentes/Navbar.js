6d13eb6be87450910c0d5298b9953fb723b91168
Amjel2022 <amjel105@gmail.com>
2023-01-16 17:06:59 -0300

    correccion menu responsive / click cerrar menu ok!

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -10,55 +10,56 @@ function Navbar() {
     
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive_nav"); 
-    }    
+    }
+
     return(
         <header>
             <h1>tabla<b> periódica</b></h1>
 
-            <nav className="navbar" ref={navRef}>
-                <Link to="/" className="links">
+            <nav className="navbar" ref={navRef} >
+                <Link to="/" className="links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-home-3-line" ></i>
                     </div>
                     Home
                 </Link>
                 
-                <Link to="/introduccion" className= "links" >
+                <Link to="/introduccion" className= "links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-bookmark-line" ></i>
                     </div>
                     Introducción
                 </Link>
                 
-                <Link to="/historia" className= "links" >
+                <Link to="/historia" className= "links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-calendar-check-line" ></i>
                     </div>
                     Historia
                 </Link>
                 
-                <Link to="/metales" className= "links" >
+                <Link to="/metales" className= "links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-layout-top-2-line" ></i>
                     </div>
                     Metales
                 </Link>
                 
-                <Link to="/NoMetales" className= "links" >
+                <Link to="/NoMetales" className= "links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-layout-left-2-line" ></i>
                     </div>
                     No Metales
                     </Link>
                 
-                <Link to="/metaloides" className= "links" >
+                <Link to="/metaloides" className= "links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-layout-right-2-line" ></i>
                     </div>
                     Metaloides
                     </Link>
                 
-                <Link to="/curiosidades" className= "links" >
+                <Link to="/curiosidades" className= "links" onClick={showNavbar}>
                     <div className="iconLinks">
                         <i className="ri-medal-2-line" ></i>
                     </div>
@@ -79,3 +80,4 @@ function Navbar() {
 }
 
 export default Navbar
+
